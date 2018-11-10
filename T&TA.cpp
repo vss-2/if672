18cab745a56e7477448e699dfdf490e4a0f54321
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-11-10 19:50:28 -0300

    Entrada compreendida
    Bem, esse passo foi difícil. O que está funcionando até então: estamos fazendo corretamente a inserção dos dados no Struct "no". Estamos também inserindo corretamente no Struct do Heap, podendo então os dados serem printados. O que precisamos agora é fazer corretamente o Djisktra, ou seja, precisamos agora criar uma lista de adjacência que vá "carimbando". Precisamos agora colocar a informação do menor caminho em alguma estrutura e jogá-la no Djisktra salvando noutro array.

--- T&TA.cpp
+++ T&TA.cpp
@@ -1,124 +1,84 @@
+#include <iostream>
 #include <stdio.h>
 
 using namespace std;
 
 double prioridade = 0;
 int N, M, mcount, X, Y, B, C, S, T;
-int percorrer;
 
-double W;
-
-int resposta = 0;
-
-struct no{
-    int prioridade, valor;
-    no *proximo;
+struct no{  
+  int origem, destino;
+  double peso;
 };
 
-struct filaPrioridade {
-    no *inicio, *fim;
-    int tamanho;
+struct heap{
+  no ligacoes[5000];
 };
 
-struct trupla {
-    int indo, chegando, peso;
-};
-
-filaPrioridade *inserir(filaPrioridade *pq, trupla valores){
-    no *auxiliar = new no();
-    auxiliar->valor = valores->peso;
-    auxiliar->proximo = NULL;
-    if(pq->inicio == NULL){
-        q->inicio = q->fim = aux;
-        pq->tamanho = pq->tamanho + 1;
-        return pq;
-    } else {
-        percorrer = 0;
-        while (percorrer < pq->tamanho - 1){
-            if (valores->peso > pq->no->proximo->prioridade){
-                no temporario = new no();                       // Crio novo nó
-                temporario->proximo = pq->no->proximo;          // temporario aponta para o resto da fila
-                pq->no->proximo = temporario;                   // próximo aponta para o temporario
-                return pq;
-            }
-            percorrer++;
-        }
-    }
+no *montarNo(no *nov, int ori, int dest, double pes){
+  nov->origem = ori;
+  nov->destino = dest;
+  nov->peso = pes;
+  return nov;
 }
 
-/*
-    Qual é a ideia?
-    Inserir, temos dois casos, está vazio a fila ou não.
-    No primeiro caso, somente preencher.
-    No segundo caso, temos que percorrer a fila, até 
-    encontrar o caso que a prioridade é maior.
-*/
-
-bool dijkstra(int S, int T){
-    filaPrioridade avisitar = new filaPrioridade();
-    
-    bool caminho = false;
-    int pesoV, atual, filho, pesoAtual;
-
-    avisitar.colocar(make_pair(minDist[S], S));
-
-    pair <int, int> auxiliar;
-
-    while (!avisitar.estavazia()){
-        auxiliar = avisitar.dotopo(); avisitar.popar();
-        atual = auxiliar.second();
-
-        for (auto auxiliar : ){
-            pesoV = auxiliar.first;
-            filho = auxiliar.second;
-            if (filho == ){
-                caminho = true;
-            }
-
-            if(minDist[atual] + pesoV < minDist[filho]){
-                minDist[filho] = minD[atual] + pesoV;
-                avisitar.colocar(make_pair(minDist[filho], filho));
-            }
-        }
-    
-    return caminho;
+heap *montarHeap(heap *atual, no botar, int posicao){
+  atual->ligacoes[posicao] = botar;
+  return atual;
 }
 
-int main(){
+heap *printarNo(heap *atual, int posicao){
+  cout << atual->ligacoes[posicao].origem << " " << atual->ligacoes[posicao].destino << " " << atual->ligacoes[posicao].peso << " \n";
+  return atual;
+}
 
+int main() {
     /*
-        -------------- Proposito do Main ---------------
+        ------------------- Proposito do Main --------------------
         1. Ele recebe prioridade do cliente, quantidade de
-           vértices e arestas que irá receber em seguida
+           vértices e arestas, as quais irá receber em seguida
         2. Logo após recebe ligações entre vértices,
            com velocidade, custo e faz o cálculo do peso
         3. Marca o caminho do par ordenado numa lista
-        ------------------------------------------------
+        ---------------------------------------------------------
     */
-    scanf("%d",&prioridade);
-    scanf("%i",&N);
-    scanf("%i",&M);
-    
+
+    scanf("%lf",&prioridade);
+    scanf("%d",&N);
+    scanf("%d",&M);
+
     // Pega prioridades, e entrada N e M
 
     int Xs[M];
     int Ys[M];
     double Ws[M];
-    int ladj[M][M];
+    int ladj[2][M];
 
-    // Cria array para X e Y, de tamanho N
-    // após isso recebe entradas dos arrays
+    heap *menorcaminho = new heap();
+    no *novo = new no();
 
-    for (mcount = 0; mcount < N; mcount++){
-        scanf("%i", &X[mmount]); // Origem
-        scanf("%i", &Y[mmount]); // Destino
+    for (mcount = 0; mcount < M; mcount++){
+        scanf("%i", &Xs[mcount]); // Origem
+        scanf("%i", &Ys[mcount]); // Destino
 
-        ladj[X][Y] = 1;  // Marco na lista de adjacencia
-        ladj[Y][X] = 1;  // Tenho que marcar (X,Y) e (Y,X)
+        ladj[0][M] = 1;  // Marco na lista de adjacencia
+        ladj[1][M] = 1;  // Tenho que marcar (X,Y) e (Y,X)
 
         scanf("%i", &B); // Velocidade
         scanf("%i", &C); // Custo
-        Ws[M] = (P*B + (1-P)*C)) / (B+C); // Peso
+        Ws[mcount] = (((prioridade*B + (1-prioridade)*C))/(B+C)); // Peso
+        
+        //printf("Peso calculado: %.2f \n",Ws[mcount]);
+
+        novo = montarNo(novo, Xs[mcount], Ys[mcount], Ws[mcount]);
+
+        menorcaminho = montarHeap(menorcaminho, *novo, mcount); 
+
+        printarNo(menorcaminho, mcount);
+
+        //cout << menorcaminho->ligacoes[mcount] << " \n" << " \n" << menorcaminho[mcount]->destino << " \n" << menorcaminho[mcount]->peso << " \n";
+
+        //cout << novo->origem << " \n" << novo->destino << " \n" << novo->peso << " \n";
     }
 
     // Recebe origem e destino
@@ -127,6 +87,4 @@ int main(){
         scanf("%i", &S);
         scanf("%i", &T);
     }
-
-    return 0;
 }
