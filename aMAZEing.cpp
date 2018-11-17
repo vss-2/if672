f35b4b2dfb3fd910c907c0a0e453b46a4944391d
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-11-16 23:47:49 -0300

    Upload do que eu desenvolvi até agora
    Criacao da questao

---
+++ aMAZEing.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Aresta {
+    int origem, destino;
+};
+
+struct Grafo {
+    int numVert, numArest;
+    struct Aresta* aresta;
+};
+
+struct Subconjunto {
+    int pai, rank;
+};
+
+// Funcao para encontrar um elemento i (usando tecnica de compressao)
+int buscar(int buscado){
+    if (buscado.pai != buscado){
+        buscado.pai = buscar (buscado.pai);
+    }
+    return buscado.pai;
+}
+
+// Funcao que une dois conjuntos de X e Y
+int unir(struct Subconjunto subconjuntos[], int x, int y){
+    int raizX = buscar(subconjuntos, x);
+    int raizY = buscar(subconjuntos, y);
+
+    // Une arvores de meor rank sob a arvore de maior rank (uniao por rank)
+
+    if (subsconjuntos[raizX].rank < subconjuntos[raizY].rank){
+        subconjuntos[raizX].pai = raizY;
+    } else if (subconjuntos[raizX].rank > subconjuntos[raizY].rank){
+        subconjuntos[raizY].pai = raizX;
+    } else { 
+        // Se o rank for igual, entao faca uma ser raiz e incremente +1 a outra
+        subconjuntos[raizY].pai = raizX;
+        subconjuntos[raizX].rank++;
+    }
+}
+
+int main(){
+    int K, N, M, Q, A, B; // Numero de casos, tamanho do lab, paredes internas rem, pares consultadas
+    int W[M];
+    int buscado1, buscado2;
+    scanf("%i",&K);
+    int a, b, c;
+    for (a = 0; a < K; a++) {
+        scanf("%i %i %i", &N, &M, &Q);
+        for (b = 0; b < M; b++){
+            scanf("%i", &W[M]);
+        }
+        for (c = 0; c < Q; c++){
+            scanf("%i %i", &A, &B);
+            buscado1 = buscar(A);
+            buscado2 = buscar(A);
+            if (buscado1 != buscado2){
+                printf("%i.%i 0\n", a, c);
+            } else {
+                printf("%i.%i 1\n", a, c);
+            }
+        }
+    }
+}
