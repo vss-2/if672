dc92df1b18be6d7f720673c9c61ff3cdfe737ac3
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-11-18 10:58:31 -0300

    Será o fim?
    Acho que estou muito perto de acabar

--- aMAZEing.cpp
+++ aMAZEing.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 struct Subconjunto {
-  int pai, rank, valor;
+  int pai, valor;
 };
 
 // Funcao para encontrar um elemento i (usando tecnica de compressao)
 int buscar(struct Subconjunto buscado[], int i){
-    if (buscado->pai != i){
+    if (buscado->pai != i && buscado->pai != -2){
         buscado->pai = buscar(buscado,buscado[i].pai);
     }
     return buscado->pai;
@@ -31,26 +34,48 @@ void unir(struct Subconjunto subconjuntos[], int x, int y){
     }
 }
 
+int K, N, M, Q, A, B; 
+int W, valor1, valor2;
+int a, b, c;
+int sobrescrever, celulahorizontal, celulavertical, buscado1, buscado2, contador;
+
 int main(){
-    int K, N, M, Q, A, B; 
     // Numero de casos, tamanho do lab, paredes internas rem, pares consultadas
-    int W[M];
     scanf("%i",&K);
-    int a, b, c;
     for (a = 0; a < K; a++) {
         scanf("%i %i %i", &N, &M, &Q);
-        Subconjunto Paredes[2*(N^2-N)-1]; //Todas as paredes possíveis
-        for (int tcu = 0; tcu < 2*(N^2-N)-1; tcu++){
-          Paredes[tcu].pai = 0;
+        // N Tamanho NxN do labirinto
+        // M Quantidade de paredes removidas
+        // Q Quantidade de pares testados
+        int Gama = (2*(N-1)) + 1;
+        Subconjunto caminho[N];
+        for (sobrescrever = 0; sobrescrever < N*N; sobrescrever++){
+            caminho[sobrescrever].pai = sobrescrever;
+            // Cada quadradinho é pai de si mesmo, e começa sendo assim
         }
         for (b = 0; b < M; b++){
-            scanf("%i", &W[M]);
-            Paredes[W[M]].pai = -2;
+            // Recebo paredes a serem removidas, tenho que unir o que elas separavam
+            scanf("%i", &W); contador = 0; valor1 = 0; valor2 = 0;
+            if (W % Gama > N - 2){ // Se der maior é pq é uma célula horizontal --W--
+              for (celulahorizontal = W % Gama; celulahorizontal == W; celulahorizontal += N){
+                contador++;
+              }
+              valor1 = N*contador; valor2 = valor1 + N;
+            } else {
+              // O valor é uma célula vertical
+              for (celulavertical = W % Gama; celulavertical == W; celulavertical += Gama){
+                contador++;
+              }
+              valor1 = (Gama * contador) - (2 * contador); valor2 = valor1 + 1;
+            }
+            cout << "Tenho que unir: " << valor1 << " " << valor2 << endl;
+            //unir(caminho, W);
         }
         for (c = 0; c < Q; c++){
             scanf("%i %i", &A, &B);
-            int buscado1 = buscar(Paredes, A);
-            int buscado2 = buscar(Paredes, B);
+            buscado1 = buscar(caminho, A);
+            buscado2 = buscar(caminho, B);
+            // Busco nos dois cantos se o valor pai é igual
             if (buscado1 != buscado2){
                 printf("%i.%i 0\n", a, c);
             } else {
