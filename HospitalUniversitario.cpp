09aab33605f47df1d7ef2b4e5dca9c6f0e9a193d
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-11-27 12:25:12 -0300

    Será que estou perto de finalmente terminar?
    Bastante encaminhada hein brasil

--- HospitalUniversitario.cpp
+++ HospitalUniversitario.cpp
@@ -1,132 +1,144 @@
 #include <stdio.h>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-String opcao;
+// Preciso criar módulos de Heap que sejam adaptáveis
+
+bool vazia, funcionou = false;
+string opcao;
 int S, I, U, A;   // Inputs necessários para questão
-int esq2, dir2, meio2, maior, contador;
+int esq, dir, meio, maior, menor, contador, temp, NXT;
 
-int temp, bubblerUpper, esq, dir, meio, remover;  // Valores que uso na heap, 
-                                                  // criados aqui pra não comprometer tempo de processamento
 
-struct Heap {
-    int* heap;
-    int tamanho;
-    int tamanho_heap;
 
-    int retornaPai(int node){
-        if (node == 0){
-            return 0;
-        } else {
-            return (node - 1) / 2;
+struct dadosPaciente{
+    int Idade, Urgencia, Servico;
+
+    dadosPaciente(int I, U, A){
+        Idade = I;
+        Urgencia = U;
+        Servico = A;
+    }
+};
+
+struct Fila{
+    dadosPaciente cabecaFila;
+    Fila* proximo;
+
+    Fila(dadosPaciente entrou){
+        cabecaFila = entrou;
+    }
+};
+
+struct Heap{
+    dadosPaciente* naHeap;
+    int tamanhoHeap, tamanhoInserir;
+
+    Heap(int inputTamanho, dadosPaciente daVez){
+        tamanhoHeap = inputTamanho;
+        heap = new int[tamanhoHeap];
+        naHeap = daVez;
+        tamanhoInserir = tamanhoHeap;
+    }
+
+    // retornaPai = (node - 1)/2;
+    // retornaEsq = (2 * node + 1);
+    // retornaDir = (2 * node + 2);
+
+    // Obs: Os heapify estão recebendo como
+    // parâmetro os valores node, mas na verdade
+    // eles recebem valores posicao, a qual devem
+    // fazer o heapify
+
+    void heapifyUpMax(int node){ // MaxHeap
+        if (node > 0 && heap[(node - 1)/2] < heap[node]){
+            swap(heap[node],heap[(node - 1)/2]);
+            heapifyUpMax(heap[(node - 1)/2]);
         }
     }
 
-    int retornaEsq(int node){
-        return (2 * node + 1);
-    }
+    void heapifyDownMax(int node){ // MaxHeap
+        esq = heap[(2 * node)+ 1];
+        dir = heap[(2 * node)+ 2];
+        maior = node;
 
-    int retornaDir(int node){
-        return (2 * node + 2);
-    }
-
-    // Usado na MaxHeap
-    void heapifyDown(int node){
-        int esq = retornaEsq(node);
-        int dir = retornaDir(node);
-        int maior = node;
-
-        if (esq < tamanho_heap && heap[esq] > heap[node]){
+        if(esq < tamanhoHeap && heap[(2 * node)+ 1] > heap[node]){
             maior = esq;
         }
-        if (dir < tamanho_heap && heap[dir] > heap[maior]){
+
+        if(dir < tamanhoHeap && heap[(2 * node)+ 2] > heap[maior]){
             maior = dir;
         }
-        if (maior != node){
+
+        if(maior != node){
             swap(node, maior);
-            heapifyDown(maior);
+            heapifyDownMax(maior);
         }
     }
 
-    void heapifyUp(int node){
-        if (node > 0 && retornaPai(node) < heap[node]){
-            swap(heap[node], heap[(node - 1)/2]);
-            heapifyUp(heap[(node - 1)/2]);
-        }
-    }
-
-    Heap(int inputTamanho){         // Construtor padrão
-        tamanho = inputTamanho;
-        heap = new int[tamanho];
-        tamanho_heap = 0;
-    }
-
-    void dobrarEspacoHeap(){        // Duplica o espaço, caso heap fique cheia
+    void dobrarEspacoHeap(){
         int* auxiliar = heap;
-        tamanho = tamanho * 2;
-        heap = new int[tamanho];
-        for (temp = 0; temp < tamanho; temp++){
+        tamanhoHeap = tamanho * 2;
+        heap = new int [tamanhoHeap];
+        for(temp = 0; temp < tamanhoHeap; temp++){
             heap[temp] = auxiliar[temp];
         }
     }
 
-    void trocarValores(int* valor1, int* valor2){   // Troca valores inseridos
-        int trocatemp = *valor1;
-        *valor1 = *valor2;
-        *valor2 = trocatemp;
-    }
-
-    void bubble_Up(){                   // Faz valores "subirem"
-        bubblerUpper = tamanho_heap - 1;
-        while (bubblerUpper > 0 && heap[bubblerUpper] <= heap[(bubblerUpper-1)/2]){
-            trocarValores(&heap[bubblerUpper], &heap[(bubblerUpper-1)/2]);
-        }
-    }
-
-    void inserirHeap(int inserido){     // Insere valores na heap
-        if (tamanho_heap){
+    void inserirHeap(int node){
+        if(tamanhoInserir <= 0){
             dobrarEspacoHeap();
         }
-        heap[tamanho_heap] = inserido;
-        tamanho_heap++;
-        bubble_Up();
+        heap[tamanhoInserir] = node;
+        //heapifyUpMin(tamanhoInserir);
+        //heapifyUpMax(tamanhoInserir);
     }
 
-    void heapify(int arrepiado){        // Arruma a heap a partir de um dado valor
-        meio = arrepiado;
-        esq = 2*arrepiado + 1;
-        dir = 2*arrepiado + 2;
-
-        if (esq < tamanho_heap && heap[esq] <= heap[meio]){
-            meio = esq;
-        }
-
-        if (dir < tamanho_heap && heap[dir] <= heap[meio]){
-            meio = dir;
-        }
-
-        if (meio != arrepiado){
-            trocarValores(&heap[arrepiado], &heap[tamanho_heap - 1]);
-            heapify(meio);
+    void heapifyUpMin(int node){
+        if(node > 0 && heap[(node - 1)/2] > heap[node]){
+            swap(node, (node - 1)/2);
+            heapifyUpMin(heap[(node - 1)/2]);
         }
     }
 
-    int removerdaHeap(){ // Retorna o valor excluido
-        if (!estaVazia){
-            remover = heap[0];
-            trocarValores(&heap[0], &heap[tamanho_heap -1]);
-            tamanho_heap--;
-            heapify(0);
-            return remover;
+    void heapifyDownMin(int node){
+        esq = (2 * node) + 1;
+        dir = (2 * node) + 2;
+        menor = node;
+
+        if(esq < tamanhoHeap && heap[(2 * node) + 1] < heap[node]){
+            menor = esq;
+        }
+
+        if(dir < tamanhoHeap && heap[(2 * node) + 2] < heap[menor]){
+            menor = dir;
+        }
+
+        if(node != menor){
+            swap(node, menor);
+            heapifyDownMin(menor);
+        }
+    }
+
+    void removerdaHeap(){
+        if(!estaVazia){
+            /*
+                remover = heap[0];
+                trocarValores(&heap[0], &heap[tamanho_heap -1]);
+                tamanho_heap--;
+                heapify(0);
+                return remover;
+            */
+            tamanhoHeap--;
         } else {
-            printf("-1 -1 -1");
-            return -1;
+            printf("-1 -1 -1\n");
         }
     }
 
     bool estaVazia(){
-        if(tamanho_heap == 0){
+        if(tamanhoHeap == 0){
             return true;
         } else {
             return false;
@@ -134,22 +146,44 @@ struct Heap {
     }
 };
 
-
 int main(){
-    Heap *estudantes = new Heap(1000);
-    // Os estudantes tem o menor risco
-    Heap *normal = new Heap(1000);
-    // Aqui são atendidos os de maior prioridade
-    scanf("%i", &S);
-    while(S--){
-        scanf("%s", &opcao);
-        if(opcao == "CIN"){
-            scanf("%i %i %i", &I, &U, &A);
-        } else if (opcao == "NXT") {
-            scanf("%i", &A);
-        } else {
+    Heap *estudantesUrgencia = new Heap(500);
+    Hera *estudantesIdade = new Heap(500);
+    // Para todos os casos estudantes CIN devem pegar
+    // apenas pacientes de baixa urgencia e idade
 
+    Heap *doutoresUrgencia = new Heap(500);
+    Heap *doutoresIdade = new Heap(500);
+    // Para todos os casos normais NXT
+
+    Fila ordemChegada[S] = new Fila();
+
+    scanf("%i", &S); // Quantidade de casos teste
+
+    while(S--){
+        scanf("%s", opcao.c_str); // Estranho, mas é o necessário para pegar uma string e não um char
+
+        if(opcao == "CIN"){ // Pegar minHeap das coisas
+            scanf("%i %i %i", &I, &U, &A); // Idade, Urgencia, Servico
+            dadosPaciente *cadastro = new DadosPaciente(I, U, A);
+            estudantesUrgencia->inserirHeap(cadastro->Urgencia); // Tem que inserir na minHeap da Urgencia
+            estudantesIdade->inserirHeap(cadastro->Idade);    // Tem que inserir na minHeap da Idade
+
+            doutoresUrgencia->inserirHeap(cadastro->Urgencia);
+            estudantesidade->inserirHeap(cadastro->Idade);
+
+            ordemChegada[s]->inserirFila(cadastro);
+            // Caso houver empate entre Urgencia e Idade:
+            // Tenho que ter uma fila de ordem de chegada simples
+
+        } else if (opcao == NXT) { // Pegar maxHeap das coisas
+            scan("%s", &NXT);
+            printf(ordemChegada[NXT].cabecaFila.Servico+" "+ordemChegada[NXT].cabecaFila.Idade+" "+ordemChegada[NXT].cabecaFila.Urgencia+"\n");
+            // Atendimento do proximo paciente que houver na fila
+            // o valor de NXT deve ser igual ao valor A do paciente
+            // Preciso implementar uma funcao de remover da fila
         }
     }
+
     return 0;
 }
