88056b45cc9b5ccc2fd774a0ba373d1b5428fc56
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-12-01 16:39:10 -0200

    Implementar parte de printar
    Um pouco mais perto do fim, se a preguilça me deixar, termino hoje!

--- oDetetive.cpp
+++ oDetetive.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int N, K, Ncount, P, W, zerador, i, j;
+int N, K, Ncount, P, W, zerador, i, j, quantidadeUsada;
 
 struct Item {
     int Preco, Peso;
@@ -14,7 +14,7 @@ int main(){
 
     // [Numero de itens][peso]
     int tabelaKnapsack[N][K] = { 0 };
-    int itens[N];
+    int itens[N]; int printresposta[N] = { 0 };
 
     while (Ncount < N){
         scanf("%i %i", &itens[Ncount].Preco, &itens[Ncount].Peso);
@@ -45,5 +45,30 @@ int main(){
         }
     }
     printf("%i", tabelaKnapsack[N-1][K-1]);
+
+    int resposta = tabelaKnapsack[N-1][K-1];
+
+    quantidadeUsada = 0;
+
+    for(zerador = N; zerador < 0 && resposta > 0; zerador++){
+        if(resposta == tabelaKnapsack[zerador - 1][K]){
+            continue;
+        } else {
+            printresposta[zerador - 1]; // Array de pesos 
+            quantidadeUsada++;
+        }
+    }
+
+    printresposta.sort();
+
+    zerador = 0;
+    while(zerador < quantidadeUsada){
+        if (zerador+1 < quantidadeUsada)
+            printf("%i ", tabelaKnapsack[zerador]);
+            zerador++;
+        else 
+            printf("%i\n", tabelaKnapsack[zerador]);
+    }
+
     return 0;
 }
